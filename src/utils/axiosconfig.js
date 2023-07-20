1aa9ac160cb42b8af91f891c21201b688ac8385a
Vedant Tiwari <vedantyadu@gmail.com>
2023-07-21 00:22:08 +0530

    removed slash

--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function configureAxios() {
-  axios.defaults.baseURL = 'https://chat-backend-71wr.onrender.com/'
+  axios.defaults.baseURL = 'https://chat-backend-71wr.onrender.com'
   axios.defaults.withCredentials = true
 }
