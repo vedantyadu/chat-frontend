f769ff71e104a5ef0821b35b4ff8139b53afca72
Vedant Tiwari <vedantyadu@gmail.com>
2023-07-20 23:50:42 +0530

    changed url

--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function configureAxios() {
-  axios.defaults.baseURL = 'http://localhost:3000'
+  axios.defaults.baseURL = 'https://chat-backend-71wr.onrender.com:3000'
   axios.defaults.withCredentials = true
 }
