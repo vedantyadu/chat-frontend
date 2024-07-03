d7ed7a46a14140ea1440c71b89aec8b9f2bc81ab
Vedant Tiwari <vedantyadu@gmail.com>
2024-07-03 14:41:49 +0530

    Update axiosconfig.js

--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function configureAxios() {
-  axios.defaults.baseURL = 'https://139.59.240.247:3000'
+  axios.defaults.baseURL = 'http://139.59.240.247:3000'
   axios.defaults.withCredentials = true
 }
