172d505d57d440da2343205b64c3c69c41318e49
Vedant Tiwari <vedantyadu@gmail.com>
2024-07-04 00:40:21 +0530

    Update axiosconfig.js

--- src/utils/axiosconfig.js
+++ src/utils/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function configureAxios() {
-  axios.defaults.baseURL = 'http://139.59.240.247:3000'
+  axios.defaults.baseURL = 'https://chatappbackend.vedantyadu.online'
   axios.defaults.withCredentials = true
 }
